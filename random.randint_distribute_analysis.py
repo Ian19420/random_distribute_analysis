61c532bbdc2a61b32853cd0200acc31ff3cbc1e5
Ian <glps1004@gmail.com>
2022-02-19 22:14:38 +0800

    second version

--- random.randint_distribute_analysis.py
+++ random.randint_distribute_analysis.py
@@ -1,6 +1,7 @@
-import random 
+import random
+#取範圍1~10是否為平均分佈 
 list_num = [random.randint(1,10) for _ in range(10000)]
-dict_num = {n: list_num.count(n) for n in list_num}
-for i in dict_num.keys():
+dict_num = {n: list_num.count(n) for n in set(list_num)}
+for i in sorted(dict_num.keys()):
 	print(f"{i}有{dict_num[i]}個")
 
